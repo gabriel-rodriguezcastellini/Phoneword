f107154489c69352450285f90e968bc7fae9df92
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2024-07-29 22:08:44 -0300

    Add project files.

---
+++ Core/PhonewordTranslator.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace Core;
+
+public static class PhonewordTranslator
+{
+    public static string? ToNumber(string raw)
+    {
+        if (string.IsNullOrWhiteSpace(raw))
+        {
+            return null;
+        }
+
+        raw = raw.ToUpperInvariant();
+
+        StringBuilder newNumber = new();
+        foreach (char c in raw)
+        {
+            if (" -0123456789".Contains(c))
+            {
+                _ = newNumber.Append(c);
+            }
+            else
+            {
+                int? result = TranslateToNumber(c);
+                if (result != null)
+                {
+                    _ = newNumber.Append(result);
+                }
+                // Bad character?
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        return newNumber.ToString();
+    }
+
+    private static readonly string[] _digits = [
+        "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+    ];
+
+    private static int? TranslateToNumber(char c)
+    {
+        for (int i = 0; i < _digits.Length; i++)
+        {
+            if (_digits[i].Contains(c))
+            {
+                return 2 + i;
+            }
+        }
+        return null;
+    }
+}
---
+++ Phoneword/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace Phoneword;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private string _translatedNumber = null!;
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        _translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber)!;
+
+        if (!string.IsNullOrEmpty(_translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Call " + _translatedNumber;
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+
+    private async void OnCall(object sender, EventArgs e)
+    {
+        if (await DisplayAlert(
+            "Dial a Number",
+            "Would you like to call " + _translatedNumber + "?",
+            "Yes",
+            "No"))
+        {
+            try
+            {
+                if (PhoneDialer.Default.IsSupported)
+                {
+                    PhoneDialer.Default.Open(_translatedNumber);
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+            }
+            catch (Exception)
+            {
+                // Other error has occurred.
+                await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+            }
+        }
+    }
+}
